9019d9523956280616f1e3acb74d9ec347fb010f
Jared Kranzler <kranzler312@gmail.com>
2018-06-18 18:12:30 -0500

    2nd commit - created some data structures and accessed some data.

--- js/app.js
+++ js/app.js
@@ -40,73 +40,91 @@ console.log("Life's A Garden, Dig IT!")
 // to represent the data, and then give a small example of the data 
 // structure:
 
-// 1. A light switch that can be either on or off.
-Booleans 
-if (lightSwitch === "on")
-  Return true;
-  else {
-    return false
-  }
+// // 1. A light switch that can be either on or off.
+// Booleans 
+// if (lightSwitch === "on")
+//   Return true;
+//   else {
+//     return false
+//   }
 
-// 2. A user's email address.
-// Objects that would store the users personal info type or "key" and then store the values user name pw ect.
-const users = {
-  email: "blah@gmail.com",
-  firstName: "Bob",
-  lastName: "Saget",
-  ect
-}
-// 3. A spaceship with a hull, laser blasters, tractor beam, and warp drive.
-an array since these are a list of characteristics or things that the spaceship has.
+// // 2. A user's email address.
+// // Objects that would store the users personal info type or "key" and then store the values user name pw ect.
+// const users = {
+//   email: "blah@gmail.com",
+//   firstName: "Bob",
+//   lastName: "Saget",
+//   ect
+// }
+// // 3. A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+// an array since these are a list of characteristics or things that the spaceship has.
 
-// 4. A list of student names from our class.
-Array
-const classList = [
-Jared,
-David,
-Ron,
-Alex,
-ect.
-]
+// // 4. A list of student names from our class.
+// Array
+// const classList = [
+// Jared,
+// David,
+// Ron,
+// Alex,
+// ect.
+// ]
 
 
 
-// 5. A list of student names from our class, each with a location.
-objects
-const studentCreepin = {
-  class: ducks,
-  students: [{ 
-  {
-  name: "jared",
-  location: "Lunchroom",
-  }
-  {
-    ect.
-  }]
+// // 5. A list of student names from our class, each with a location.
+// objects
+// const studentCreepin = {
+//   class: ducks,
+//   students: [{ 
+//   {
+//   name: "jared",
+//   location: "Lunchroom",
+//   }
+//   {
+//     ect.
+//   }]
+// };
+
+// // 6. A list of student names from our class, each with a location 
+// //    and each with a list of favorite tv shows.
+
+// objects
+// const studentCreepin = {
+//   class: funkyDucks,
+//   students: [{ 
+//   {
+//   name: "jared",
+//   location: "Lunchroom",
+//   favoriteTvShow: "the daily show"
+//   }
+//   {
+//     ect.
+//   }]
+// };
+
+
+// //=================================================================
+// //                       Take it Easy
+// //=================================================================
+// 1. Make an array that holds all of the colors of the rainbow.
+const rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+console.log(rainbow);
+// 2. Write code that will access "blue" from the rainbow array.
+console.log(rainbow[4]);
+
+// 3. Make an object that is called your name and holds the 
+// information about your favorite food, a hobby, the name of the 
+// town that you live in currently, and your favorite datatype.
+const jRock = {
+  favoriteFood: "Anything Fried",
+  hobby: "cooking",
+  city: "Chicago",
+  datatype: "html"
 };
 
-// 6. A list of student names from our class, each with a location 
-//    and each with a list of favorite tv shows.
-
-objects
-const studentCreepin = {
-  class: funkyDucks,
-  students: [{ 
-  {
-  name: "jared",
-  location: "Lunchroom",
-  favoriteTvShow: "the daily show"
-  }
-  {
-    ect.
-  }]
-};
-
-
-
-
-
-
+// 4. Write code that will access your hobby from the object that you 
+// just created.
+console.log(jRock.hobby)
 
 
 
